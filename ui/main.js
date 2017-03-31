c39e4a742c67e3491188987ffcd7afda63546f7a
anandhupb <anandhupb006@gmail.com>
2017-03-31 07:23:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,9 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 var marginleft = 0;
 function moveright () {
-    marginleft = marginright + 1;
+    marginleft = marginright + 10;
     img.style.marginleft = marginleft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
+    var interval = setInterval(moveRight, 100);
 };
